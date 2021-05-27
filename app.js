633000a09b98840016fd20f063004f707cd9c38a
code_kareem <seniorteck4real@gmail.com>
2021-05-26 21:08:57 -0400

    change todo default list text

--- app.js
+++ app.js
@@ -2,6 +2,9 @@
 const addForm = document.querySelector(".add");
 //select the ul list 
 const list = document.querySelector(".todos");
+// a reference to form search section
+const search = document.querySelector('.search input')
+
 
 //ouput the input from the addform to the ul list
 function generalTemplate(todo){
@@ -19,7 +22,7 @@ addForm.addEventListener('submit', evt=>{
     evt.preventDefault();
     //reference to the value inputed in the form section
     //the .trim() method removes all empty space in the form
-    const todo = addForm.add.value.trim();
+    const todo = addForm.add.value.trim().toLowerCase();
     //console.log(todo);
     if(todo.length){
         //calling the generalTemplate function and passing todo as a parameter/arguments
@@ -29,4 +32,32 @@ addForm.addEventListener('submit', evt=>{
     }
 
 
+});
+
+
+//delete Todos
+// listening for click event in the ul list 
+list.addEventListener('click',(evt)=>{
+    //checks if the event target contains delete in it classList
+    if (evt.target.classList.contains("delete")){
+        evt.target.parentElement.remove();
+    }
+})
+
+const filteredTodo = (userInput)=>{
+    //selecting the children of the ul
+    //Array.from turns the html collection into an array
+    Array.from(list.children)
+        .filter((todo)=>!todo.textContent.includes(userInput))
+        .forEach((todo)=>todo.classList.add('filtered'));
+
+    Array.from(list.children)
+        .filter((todo)=>todo.textContent.includes(userInput))
+        .forEach((todo)=>todo.classList.remove('filtered'));    
+}
+//search todo
+search.addEventListener('keyup',()=>{
+    const userInput = search.value.trim().toLowerCase();
+    // console.log(userInput)
+    filteredTodo(userInput)
 })
